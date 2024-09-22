3b921a9605bab5444a44a5c911c920405a114906
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-22 11:44:29 +0200

    getting a response back from the localhost using express.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // jshint ESversion:6
 
 // const express = require(`express`);
-import express from "express";
+import express, { response } from "express";
+import { request } from "http";
 
 const app = express();
+
+app.get("/", function (request, response) {
+    // to display the request in the terminal
+    console.log(request)
+
+    // to send a respond to show on the browser
+    response.send("<h1>Hello there 👋</h1>");
+});
 
 app.listen(3000, function(){
     console.log(`server started at port: 3000`);
