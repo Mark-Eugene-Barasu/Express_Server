0312f3e150330f3ad4ca2ab0d7f1336ee87a25ed
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-22 11:17:46 +0200

    testing server port

---
+++ server.js
@@ -0,0 +1,9 @@
+
+const express = require(`express`);
+import express from `express`;
+
+const app = express();
+
+app.listen(3000, function() {
+    console.log(`server started on port: 3000`);
+});
